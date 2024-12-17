cbc6df60723ae0a1c7c99c44ca8d9ad6068c8658
Adam Akuev <akuev2005@mail.ru>
2024-12-17 16:53:45 +0300

    Initial commit

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -1,31 +1,37 @@
-﻿#include "pch.h" // Добавьте эту строку
+﻿#include "pch.h"
 #include <iostream>
 #include <cmath>
 
-// Структура для представления комплексного числа
 struct Complex {
-    double real; // Действительная часть
-    double imag; // Мнимая часть
+    double real;
+    double imag;
 };
 
 namespace MathLib {
-    // Функция для сложения комплексных чисел
     Complex add(const Complex& a, const Complex& b) {
         return { a.real + b.real, a.imag + b.imag };
     }
 
-    // Функция для вычитания комплексных чисел
     Complex subtract(const Complex& a, const Complex& b) {
         return { a.real - b.real, a.imag - b.imag };
     }
 
-    // Функция для умножения комплексных чисел
     Complex multiply(const Complex& a, const Complex& b) {
         return { a.real * b.real - a.imag * b.imag, a.real * b.imag + a.imag * b.real };
     }
 
-    // Функция для вычисления модуля комплексного числа
     double modulus(const Complex& a) {
         return std::sqrt(a.real * a.real + a.imag * a.imag);
+    }
+
+    Complex divide(const Complex& a, const Complex& b) {
+        if (b.real == 0 && b.imag == 0) {
+            throw ("Деление на ноль невозможно!!!");
+        }
+        double denominator = b.real * b.real + b.imag * b.imag;
+        return {
+            (a.real * b.real + a.imag * b.imag) / denominator,
+            (a.imag * b.real - a.real * b.imag) / denominator
+        };
     }
 }
